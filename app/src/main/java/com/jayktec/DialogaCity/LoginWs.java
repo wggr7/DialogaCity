614bbe0500852df0ea01f9f5855a46b277ed9af3
wggr7 <wilmer11792@gmail.com>
2017-03-27 16:13:11 -0400

    actualizando login

--- app/src/main/java/com/jayktec/DialogaCity/LoginWs.java
+++ app/src/main/java/com/jayktec/DialogaCity/LoginWs.java
@@ -3,10 +3,21 @@ package com.jayktec.DialogaCity;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.ksoap2.SoapEnvelope;
 import org.ksoap2.serialization.SoapObject;
 import org.ksoap2.serialization.SoapSerializationEnvelope;
 import org.ksoap2.transport.HttpTransportSE;
+
+import java.io.UnsupportedEncodingException;
 
 /**
  * Created by yisheng on 20-Oct-15.
@@ -42,11 +53,85 @@ public class LoginWs extends AsyncTask<String,Void, Boolean> {
     @Override
     protected Boolean doInBackground(String... params) {
         boolean result = false;
-        SoapObject resultsRequestSOAP;
         String login = params[0];
         String clave = params[1];
 
-        final String NAMESPACE = "http://AppMobile/";
+
+        HttpClient httpClient = new DefaultHttpClient();
+
+        HttpPost post =
+                new HttpPost("http://jayktec.com.ve/DialogaWeb/servicios/autenticarWowzer");
+
+        post.setHeader("content-type", "application/json");
+
+        JSONObject dato = new JSONObject();
+
+        try {
+            dato.put("user", login);
+            dato.put("pass", clave);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        Log.i("ws", login);
+        Log.i("ws", clave);
+        Log.i("ws", dato.toString());
+        StringEntity entity = null;
+        try {
+            entity = new StringEntity(dato.toString());
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        post.setEntity(entity);
+
+        Log.d("ws", String.valueOf(post.getEntity()));
+
+        try
+        {
+            HttpResponse resp = httpClient.execute(post);
+            Log.i("ws", resp.toString());
+            String respStr = EntityUtils.toString(resp.getEntity());
+            Log.i("ws", respStr);
+            if(!respStr.contains("false")){
+                JSONArray respJSON = new JSONArray(respStr);
+
+
+                idRegistro = respJSON.getInt(Integer.parseInt("idWowzer"));
+                alias = respJSON.getString(Integer.parseInt("wowLogin")).toString();
+                nivel = respJSON.getInt(Integer.parseInt("wowPuntos"));
+                if(respJSON.getString(Integer.parseInt("wowSexo")).toString().equals("M"))
+
+                {
+                    sexo="M";
+                }
+                else
+                {
+                    sexo="F";
+                }
+
+                result=true;
+                trabajando=false;
+            }
+            else{
+                trabajando=false;
+                result=false;
+            }
+
+
+        }
+        catch(Exception ex)
+        {
+            trabajando=false;
+            Log.e("ServicioRest","Error!", ex);
+            result=false;
+        }
+
+
+
+
+
+
+       /* final String NAMESPACE = "http://AppMobile/";
         final String URL="http://www.jayktec.com.ve:8080/AplicacionWebServices/UsuarioMobile?wsdl";
         final String METHOD_NAME = "VerificacionLogin";
         final String SOAP_ACTION = "http://www.jayktec.com.ve:8080/AplicacionWebServices/UsuarioMobile";
@@ -127,7 +212,7 @@ public class LoginWs extends AsyncTask<String,Void, Boolean> {
             result = false;
         }
         trabajando=false;
-
+*/
         return result;
     }
 
